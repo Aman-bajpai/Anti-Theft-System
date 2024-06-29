6e58924b6e56b8ffa3d42f0ab4b00d3a05d1a5d5
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2024-06-29 12:29:19 +0530

    added the signUp component and its functionality

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role:""
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await axios.post(
+        "http://localhost:3000/api/user/auth/signup",
+        formData
+      );
+      const data = res.data; // Simplified
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+      }
+      navigate("/sign-in");
+    } catch (error) {
+      console.error("There was an error submitting the form:", error);
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          value={formData.username}
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          value={formData.email}
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={formData.password}
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-75"
+        >
+          {loading ? "loading..." : "sign up"}
+        </button>
+      </form>
+      <div className="flex gap-3 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt5">{error && "Something went wrong"}</p>
+    </div>
+  );
+};
+
+export default SignUp;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 import userAuthRoutes from "./routes/userAuth.route.js";
 
 dotenv.config();
@@ -19,6 +20,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ credentials: true }));
+app.use(cookieParser());
 
 app.use("/api/user/auth", userAuthRoutes);
 
