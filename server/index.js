bf02edccc44df0fb3b9738368d09ef6ab915f9a1
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2024-06-29 11:47:44 +0530

    installed all the necessary dependencies and made all the necessary files

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from 'cors';
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to the MongoDB database");
+  })
+  .catch((error) => {
+    console.log("ERR--> ", error);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ credentials: true }));
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000!");
+});
